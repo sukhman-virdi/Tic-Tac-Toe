11b3574bdc233b3ad9ce5691485aeac3c617bd55
urquidialberto <urquidialberto5@gmail.com>
2026-03-24 12:50:46 -0700

    Update adminSignUp.php

--- CPSC2221_final/html/adminSignUp.php
+++ CPSC2221_final/html/adminSignUp.php
@@ -15,8 +15,10 @@ $Level    = $_POST['Level'];
  
 // Input validation
 if (empty($AdminID) || empty($Name) || empty($Email) || empty($Password) || empty($Level)) {
-	echo "Error: All fields are required. <br>";
-	echo "<a class='btn btn-secondary mt-2' href='adminSignUp.html'>Go Back</a>";
+	echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'>";
+	echo "<div class='container mt-4'>";
+	echo "<div class='alert alert-danger'>Error: All fields are required.</div>";
+	echo "<a class='btn btn-secondary' href='adminSignUp.html'>Go Back</a></div>";
 	exit;
 }
  
